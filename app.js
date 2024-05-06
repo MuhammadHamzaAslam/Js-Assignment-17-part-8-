790241ed37ce393d2ebb6eb2cd7c5f86a2c5b650
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-05-06 20:14:01 +0500

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+function java() {
+    let user = prompt('Enter ').toLowerCase()
+if (user.slice(0,0+4) == 'java') {
+alert('Its start with Java') 
+}
+else {
+  alert('It doesnot start with java')
+}
+}
